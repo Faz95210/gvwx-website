81dc3334728cc22f89f62ef8eed237a3f289871e
Fawzi Bedidi <fawzi.bedidi@nconcepts.fr>
2020-03-19 13:47:29 +0800

    fixes

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -7,11 +7,112 @@ require dirname(__FILE__) . '/../../vendor/yiisoft/yii2/Yii.php';
 require dirname(__FILE__) . '/../../common/config/bootstrap.php';
 require dirname(__FILE__) . '/../config/bootstrap.php';
 
-$config = yii\helpers\ArrayHelper::merge(
-    require dirname(__FILE__) . '/../../common/config/main.php',
-    require dirname(__FILE__) . '/../../common/config/main-local.php',
-    require dirname(__FILE__) . '/../config/main.php',
-    require dirname(__FILE__) . '/../config/main-local.php'
-);
+$config = [
+    'id' => 'app-frontend',
+    'name' => 'AuctionManager',
+    'basePath' => dirname(dirname(__FILE__)),
+    'controllerNamespace' => 'frontend\controllers',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm' => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(dirname(__FILE__))) . '/vendor',
+    'bootstrap' => ['debug', 'gii', 'log'],
+    'modules' => [
+        'debug' => [
+            'class' => 'yii\debug\Module',
+        ],
+        'gii' => [
+            'class' => 'yii\gii\Module',
+        ]
+    ],
+    'components' => [
+        'request' => [
+            'cookieValidationKey' => 'cpI6c9283FRIxaSjZUoFkrzFxsP0XvkA',
+            'csrfParam' => '_csrf-frontend',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'advanced-frontend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=auctionmnqroot.mysql.db;dbname=auctionmnqroot',
+            'username' => 'auctionmnqroot',
+            'password' => 'Okwolp2ok',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            // 'useFileTransport' => true,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'encryption' => 'tls',
+                'host' => 'smtp.gmail.com',
+                'port' => '587',
+                'username' => 'gvxconseil@gmail.com',
+                'password' => '',
+            ],
+        ],
+        'i18n' => [
+            'translations' => [
+                'navbar*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                ],
+                'login*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                ],
+                'menu*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                ],
+                'frontend*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                ],
+                'backend*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                ],
+                'api*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                ],
+            ],
+        ],
+    ],
+
+    // set target language to be Russian
+    'language' => 'fr-FR',
+
+    // set source language to be English
+    'sourceLanguage' => 'fr-FR',
+];
 
 (new yii\web\Application($config))->run();
