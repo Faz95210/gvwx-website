0934f6832cfb4e7f5e3fb8a48532f4544c12213e
Fawzi Bedidi <fawzi.bedidi@nconcepts.fr>
2020-05-04 17:13:17 +0800

    icon

--- frontend/web/navbar/navbar.php
+++ frontend/web/navbar/navbar.php
@@ -72,7 +72,7 @@ use yii\helpers\Html;
             <ul class="metismenu" id="side-menu">
                 <!--<li class="menu-title">Main</li> -->
                 <li>
-                    <?= Html::a('<i class="ti-home"></i><!--<span class="badge badge-primary badge-pill float-right">2</span> --><span> ' . \Yii::t('menu', 'Items') . '</span>', ['item/get'], ['class' => 'waves-effect']) ?>
+                    <?= Html::a('<svg color="green" fill="green" width="20px" height="20px"><image  style="width: 100%" href="images/article.svg"/></svg><span> ' . \Yii::t('menu', 'Items') . '</span>', ['item/get'], ['class' => 'waves-effect']) ?>
                     <!--
                                 <a href="index.php" class="waves-effect">
                                     <i class="ti-home"></i><span> <?php echo \Yii::t('menu', 'Items'); ?> </span>
